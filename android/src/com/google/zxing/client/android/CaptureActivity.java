2ebb385b1ef74b96018b9233f829f498a51cdba6
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-28 16:14:48 +0200

    allow rotation

--- android/src/com/google/zxing/client/android/CaptureActivity.java
+++ android/src/com/google/zxing/client/android/CaptureActivity.java
@@ -124,14 +124,6 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
     handler = null;
     lastResult = null;
 
-    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-    if (prefs.getBoolean(PreferencesActivity.KEY_DISABLE_AUTO_ORIENTATION, true)) {
-      setRequestedOrientation(getCurrentOrientation());
-    } else {
-      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
-    }
-
     resetStatusView();
 
     SurfaceView surfaceView = (SurfaceView) findViewById(R.id.preview_view);
